5032477855fce987cc4892276a4f9449ce63ac65
root <root@DESKTOP-MBDMDNQ>
2025-05-12 03:08:51 +0500

    fixed further conflicts

--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-const {
-  getUsers,
-  getUserById,
-  createUser,
-  updateUser,
-  patchUser,
-} = require('../controllers/user.controller');
-
-router.get('/', getUsers);
-router.get('/:id', getUserById);
-router.post('/', createUser);
-router.put('/:id', updateUser);
-router.patch('/:id', patchUser);
-
-module.exports = router;
-=======
 const express = require('express');
 const auth = require("../middleware/auth");
 
@@ -30,12 +11,11 @@ const {
   getSavedJobs
 } = require('../controllers/user.controller');
 
-router.get('/',auth, getUsers);          
-router.get('/:id',auth, getUserById); 
-router.post('/',auth, createUser);   
-router.put('/:id',auth, updateUser);    
-router.patch('/:id',auth, patchUser); 
-router.get('/:userId/saved-jobs',auth, getSavedJobs);
+router.get('/', auth, getUsers);
+router.get('/:id', auth, getUserById);
+router.post('/', auth, createUser);
+router.put('/:id', auth, updateUser);
+router.patch('/:id', auth, patchUser);
+router.get('/:userId/saved-jobs', auth, getSavedJobs);
 
 module.exports = router;
->>>>>>> 94b17bf0681ccd7648c14468b2742919ae96c24c
--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-const express = require("express")
-const mongoose = require("mongoose")
-const jobRouters = require("./routes/job.router")
-const proposalRoutes = require('./routes/proposal.router');
-const userRoutes = require('./routes/user.router');
-const dotenv = require("dotenv")
-
-const app = express();
-dotenv.config();
-
-const port = process.env.PORT || 5001
-const connectionString = process.env.MONGO_URL //running on localhost:5000 currently
-
-// middlewares
-app.use(express.json());
-
-
-// routes middlewares
-app.use('/jobs', jobRouters);
-app.use("/proposals", proposalRoutes);
-app.use("/users", userRoutes);
-
-mongoose.connect(connectionString)
-    .then(() => {
-        console.log("database connected successfully");
-        app.listen(port, () => {
-            console.log("server is running at port ", port)
-        })
-    })
-    .catch((e) => {
-        console.log("database not connected");
-        console.log(e.message);
-    })
-=======
 const express = require("express")
 const mongoose = require("mongoose")
 const jobRouters = require("./routes/job.router")
@@ -51,26 +16,25 @@ const connectionString = process.env.MONGO_URL
 // middlewares
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
+    origin: 'http://localhost:5173',
+    credentials: true
 }));
 
 
 // routes middlewares
-app.use('/jobs',jobRouters);
-app.use("/proposals",proposalRoutes);
-app.use("/users",userRoutes);
+app.use('/jobs', jobRouters);
+app.use("/proposals", proposalRoutes);
+app.use("/users", userRoutes);
 app.use(authRoutes);
 
 mongoose.connect(connectionString)
-.then(()=>{
-    console.log("database connected successfully");
-    app.listen(port,()=>{
-        console.log("server is running at port ",port)
+    .then(() => {
+        console.log("database connected successfully");
+        app.listen(port, () => {
+            console.log("server is running at port ", port)
+        })
     })
-})
-.catch((e)=>{
-    console.log("database not connected");
-    console.log(e.message);
-})
->>>>>>> 94b17bf0681ccd7648c14468b2742919ae96c24c
+    .catch((e) => {
+        console.log("database not connected");
+        console.log(e.message);
+    })
